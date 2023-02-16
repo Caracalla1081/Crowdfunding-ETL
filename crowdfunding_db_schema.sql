fa625889b579386fb73bd2e29ca356df703f19ee
Caracalla1081 <Caracalla1081@gmail.com>
2023-02-15 20:46:09 -0500

    Updated crowdfunding files and added png

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -22,6 +22,16 @@ CREATE TABLE "campaign" (
      )
 );
 
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
 CREATE TABLE "category" (
     "category_id" varchar(10)   NOT NULL,
     "category_name" varchar(50)   NOT NULL,
@@ -38,22 +48,26 @@ CREATE TABLE "subcategory" (
      )
 );
 
-CREATE TABLE "contacts" (
-    "contact_id" int   NOT NULL,
-    "first_name" varchar(50)   NOT NULL,
-    "last_name" varchar(50)   NOT NULL,
+CREATE TABLE "backers" (
+    "backer_id" varchar(10)   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "first_name" varchar(100)   NOT NULL,
+    "last_name" varchar(100)   NOT NULL,
     "email" varchar(100)   NOT NULL,
-    CONSTRAINT "pk_contacts" PRIMARY KEY (
-        "contact_id"
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id","cf_id"
      )
 );
-
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "contacts" ("contact_id");
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
 REFERENCES "category" ("category_id");
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
+
+ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "campaign" ("contact_id");
+
+ALTER TABLE "backers" ADD CONSTRAINT "fk_backers_cf_id" FOREIGN KEY("cf_id")
+REFERENCES "campaign" ("cf_id");
 
